d1f90b5963c9d60d103ccec5bb9b8e4fd6ec6ca6
md-arifuzzaman <mdarifuzzaman.com@gmail.com>
2021-10-13 23:33:10 +0600

    landzai update

--- Landzai/landzai-addon/landzaielement/includes/widgets/blog/layout4.php
+++ Landzai/landzai-addon/landzaielement/includes/widgets/blog/layout4.php
@@ -1,11 +1,12 @@
 <?php
-    echo'<!-- blog area three start here  -->
-    <section class="blog-area-three section pb-90">
+    echo'<!-- blog area four start here  -->
+    <section class="blog-area-four section pb-90">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 offset-lg-2">
-                    <div class="section-title-three mb-37 text-center ">
+                    <div class="section-title-four mb-45 text-center ">
                         <h2 class="title">'.$settings['title'].'</h2>
+                        <p class="sub-title">'.$settings['info'].'</p>
                     </div>
                 </div>
             </div>
@@ -25,11 +26,11 @@
                         <div class="blog-info">
                             <h3 class="blog-title"><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></h3>
                             <ul class="blog-meta">
-                                <li><a href="'.get_day_link(get_the_time('Y'), get_the_time('m'), get_the_time('j')).'"><i class="far fa-calendar"></i> '.get_the_time('j F, Y').'</a></li>
-                                <li><a href="#"><i class="far fa-clock"></i>'.display_read_time().' Min To Read</a></li>
+                                <li><a href="'.get_day_link(get_the_time('Y'), get_the_time('m'), get_the_time('j')).'"><i class="far fa-calendar"></i>  '.get_the_time('j F, Y').'</a></li>
+                                <li><a href="#"><i class="far fa-clock"></i> '.display_read_time().' Min To Read</a></li>
                             </ul>
                             <p class="blog-cotent">' . get_the_excerpt() . '</p>
-                            <a href="' . get_the_permalink() . ' " class="blog-btn">Read More</a>
+                            <a href="' . get_the_permalink() . '" class="blog-btn">Read More</a>
                         </div>
                     </div>
                 </div>';
@@ -39,4 +40,4 @@
         echo '</div>
         </div>
     </section>
-    <!-- blog area three end here  -->';
+    <!-- blog area four end here  -->';
